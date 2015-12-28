d54214e17c7b864aa5329d06e605abeaa22d68ce
revers <revers@yandex-team.ru>
2015-12-28 18:48:14 +0300

    progress

--- src/main.js
+++ src/main.js
@@ -2,42 +2,72 @@ var path = require('path');
 
 var loaderUtils = require('loader-utils');
 
-var convergence = require('convergence');
+const bemdeclToFs = require('bemdecl-to-fs');
+
+var bemDeps = require('@bem/deps');
 
 module.exports = function(source) {
     this.cacheable();
     var callback = this.async();
     var loaderOptions = loaderUtils.parseQuery(this.query);
     var techs = loaderOptions.techs;
-    var file = loaderUtils.getRemainingRequest(this);
+    var decl = this.exec(source, this.resourcePath).deps;
+    var levels = loaderOptions.levels;
 
-    convergence({
-        decls: [file],
-        levels: loaderOptions.levels
-    })
-        .then(
-            function (data) {
-                if (techs.indexOf('yate') >= 0) {
-                    var fs = require('fs');
-                    var fileName = file + '.yate';
+    getRelations(levels)
+        .then(function (relations) {
+            var res = bemDeps.resolve(decl, relations, { tech: techs });
 
-                    fs.writeFileSync(fileName, data.filter(function (b) {
-                        return b.tech === 'yate';
-                    }).map(generateIncludeStringFromEntity).join(''));
-                }
-
-                callback(null, data.filter(function (b) {
-                    return techs.indexOf(b.tech) >= 0;
-                }).map(generateRequireStringFromEntity).join(''));
-            },
-            console.error
-        );
+            return bemdeclToFs(res.entities, levels, techs);
+        })
+        .then(function (files) {
+            callback(null, files.map(generateRequireStringFromFilePath).join('\n'));
+        }).catch(console.log);
 };
 
-function generateRequireStringFromEntity(entity) {
-    return "require('" + path.resolve(entity.path) + "');\n";
+function generateRequireStringFromFilePath(filePath) {
+    return "require('" + path.resolve(filePath) + "');";
 }
 
 function generateIncludeStringFromEntity(entity) {
     return 'include "' + path.resolve(entity.path) + '"\n';
+}
+
+function getRelations(levels) {
+    var stream = bemDeps.load({ levels: levels });
+
+    return new Promise(function (resolve, reject) {
+        // stream is already ended
+        if (!stream.readable) return resolve([]);
+
+        var arr = [];
+
+        stream.on('data', onData);
+        stream.on('end', onEnd);
+        stream.on('error', onEnd);
+        stream.on('close', onClose);
+
+        function onData(doc) {
+            arr.push(doc)
+        }
+
+        function onEnd(err) {
+            if (err) reject(err);
+            else resolve(arr);
+            cleanup();
+        }
+
+        function onClose() {
+            resolve(arr);
+            cleanup();
+        }
+
+        function cleanup() {
+            arr = null;
+            stream.removeListener('data', onData);
+            stream.removeListener('end', onEnd);
+            stream.removeListener('error', onEnd);
+            stream.removeListener('close', onClose);
+        }
+    }).catch(console.log)
 }
